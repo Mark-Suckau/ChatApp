892b2a89ec3efd9148e741ca72bef4118cd721e1
CatSittingOnKeyboard <75488663+CatSittingOnKeyboard@users.noreply.github.com>
2021-07-18 17:35:45 +0200

    restructured folders

---
+++ client/ui/mainchat.py
@@ -0,0 +1,28 @@
+import socket
+import threading
+import pygame
+from tkinter import *
+from rooms import Window1
+from mainUI import mainClass
+from mainUI import mainClass
+
+
+
+
+def rooms():
+    root = Tk()
+    window1 = Window1(root)
+
+
+
+
+rooms()
+
+
+
+
+
+
+
+
+
