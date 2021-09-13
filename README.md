513f1265c299528a58a07dbc022e45051aacd826
CatSittingOnKeyboard <Suckau.Mark.Coding@gmail.com>
2021-09-13 20:57:59 +0200

    Updated readme, dbconnector functions added for interacting with db from server

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # ChatApp
 
-<strong>Client</strong>
-<br>
+## Client
+
 Once logged into server with account, the client sends a request for all rooms the user is apart to the server of and stores their ids until logged out.
+
+### Client Online vs. Offline message receiving mode
+
+When client is online and has the program actively running, server sends live messages from rooms via TCP connection.
+When client is offline and has the program closed, it will periodically send http requests to a secondary http api running on server to receive message notifications.
+
+## Server
+
+1. Service for TCP connections which will be used in Online mode of clients (see above)
+2. Service for HTTP requests which will be used in Offline mode of clients (see above)
+
+### Database Connector
+
+Used by server to manipulate Database and properly verify clients.
+Server checks what rooms a client is apart of using DB Connector to:
+
+1. Allow gui on client side to properly display rooms when a client logs on
+2. Check if a client is permitted to receive/send messages from/to a given room whenever a client tries to receive/send a message
--- src/server/dbconnector.py
+++ src/server/dbconnector.py
@@ -2,5 +2,34 @@ import psycopg2
 
 class DB_Connector:
   def __init__(self, host, port, dbname, username, password):
-    self.conn = psycopg2.connect(host=host, port=port, dbname=dbname, username=username, password=password)
+    self.conn = psycopg2.connect(host=host,
+                                 port=port, 
+                                 dbname=dbname, 
+                                 username=username, 
+                                 password=password)
+    
+    self.cursor = self.conn.cursor()
   
+  def insert_message(self, create_date, message_body, creator_id, room_id):
+    # inserts a message into "message" table
+    self.cursor.execute('''INSERT INTO message (create_date, message_body, creator_id, room_id) 
+                        VALUES (%d, %-s, %s, %s)''', 
+                        create_date, message_body, creator_id, room_id)
+    self.conn.commit()
+    
+  def get_user_rooms(self, user_id):
+    # returns all rooms a given user is apart of (used to determine: if a user is allowed to read/write messages to this room; gives client data on what rooms to display on gui when user logs on)
+    user_rooms = self.cursor.execute('''SELECT room_id
+                        FROM user_room
+                        INNER JOIN room
+                        ON user_room.room_id = room.room_id
+                        WHERE user_room.user_id = %s;''', 
+                        user_id)
+    return user_rooms
+  
+  def get_messages_room(self, room_id, start_date, end_date):
+    # returns all messages in a room in a specified range of dates
+    
+  def shutdown(self):
+    self.cursor.close()
+    self.conn.close()
